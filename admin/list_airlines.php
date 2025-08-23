efaa73e84ff33287ddae6708adcbcaf854a830c2
Cv_Boghara <boghara641@gmail.com>
2025-08-23 15:40:47 +0530

    Add changes 15.40

--- admin/list_airlines.php
+++ admin/list_airlines.php
@@ -14,6 +14,7 @@ if(isset($_POST['del_airlines']) and isset($_SESSION['adminId'])) {
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
     mysqli_close($conn);
+    // header('Location: list_airlines.php');
     echo("<script>location.href = 'list_airlines.php';</script>");
     exit();
   }
