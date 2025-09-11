92d191654250aa6d0d049486de22132b3b0cdaa4
Cv_Boghara <boghara641@gmail.com>
2025-09-11 22:50:00 +0530

    Add changes the Admin Login Page Functionality3

---
+++ DATABASE FILE/flightnest_structure.sql
@@ -0,0 +1,470 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 05, 2022 at 06:36 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `flightnest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_uname` varchar(20) NOT NULL,
+  `admin_email` varchar(50) NOT NULL,
+  `admin_pwd` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `airline`
+--
+
+CREATE TABLE `airline` (
+  `airline_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `seats` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cities`
+--
+
+CREATE TABLE `cities` (
+  `city` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `cities` (`city`) VALUES
+('Mumbai'),
+('Delhi'),
+('Bengaluru'),
+('Ahmedabad'),
+('Surat'),
+('Rajkot'),
+('Hyderabad'),
+('Pune'),
+('Kolkata'),
+('Chennai'),
+('Goa'),
+('Dubai'),
+('Sharjah'),
+('Doha'),
+('Muscat'),
+('Jeddah'),
+('London'),
+('Singapore'),
+('Bangkok'),
+('Kuwait City'),
+('Abu Dhabi'),
+('Diu'),
+('Jaipur'),
+('Kolkata'),
+('Udaipur');
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `feed_id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `q1` varchar(250) NOT NULL,
+  `q2` varchar(20) NOT NULL,
+  `q3` varchar(250) NOT NULL,
+  `rate` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `flight`
+--
+
+CREATE TABLE `flight` (
+  `flight_id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `arrivale` datetime NOT NULL,
+  `departure` datetime NOT NULL,
+  `Destination` varchar(20) NOT NULL,
+  `source` varchar(20) NOT NULL,
+  `airline` varchar(20) NOT NULL,
+  `Seats` varchar(110) NOT NULL,
+  `duration` varchar(20) NOT NULL,
+  `Price` int(11) NOT NULL,
+  `flight_code` varchar(10) NOT NULL,
+  `status` varchar(6) DEFAULT NULL,
+  `issue` varchar(50) DEFAULT NULL,
+  `last_seat` varchar(5) DEFAULT '',
+  `bus_seats` int(11) DEFAULT '20',
+  `last_bus_seat` varchar(5) DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `passenger_profile`
+--
+
+CREATE TABLE `passenger_profile` (
+  `passenger_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `flight_id` int(11) NOT NULL,
+  `mobile` varchar(110) NOT NULL,
+  `dob` datetime NOT NULL,
+  `f_name` varchar(20) DEFAULT NULL,
+  `m_name` varchar(20) DEFAULT NULL,
+  `l_name` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `card_no` varchar(16) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `flight_id` int(11) NOT NULL,
+  `expire_date` varchar(5) DEFAULT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pwdreset`
+--
+
+CREATE TABLE `pwdreset` (
+  `pwd_reset_id` int(11) NOT NULL,
+  `pwd_reset_email` varchar(50) NOT NULL,
+  `pwd_reset_selector` varchar(80) NOT NULL,
+  `pwd_reset_token` varchar(120) NOT NULL,
+  `pwd_reset_expires` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `ticket_id` int(11) NOT NULL,
+  `passenger_id` int(11) NOT NULL,
+  `flight_code` varchar(10) NOT NULL,
+  `flight_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `seat_no` varchar(10) NOT NULL,
+  `cost` int(11) NOT NULL,
+  `class` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `airline`
+--
+ALTER TABLE `airline`
+  ADD PRIMARY KEY (`airline_id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`feed_id`);
+
+--
+-- Indexes for table `flight`
+--
+ALTER TABLE `flight`
+  ADD PRIMARY KEY (`flight_id`),
+  ADD KEY `admin_id` (`admin_id`);
+
+--
+-- Indexes for table `passenger_profile`
+--
+ALTER TABLE `passenger_profile`
+  ADD PRIMARY KEY (`passenger_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `flight_id` (`flight_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`card_no`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `flight_id` (`flight_id`);
+
+--
+-- Indexes for table `pwdreset`
+--
+ALTER TABLE `pwdreset`
+  ADD PRIMARY KEY (`pwd_reset_id`);
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`ticket_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `flight_id` (`flight_id`),
+  ADD KEY `passenger_id` (`passenger_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `airline`
+--
+ALTER TABLE `airline`
+  MODIFY `airline_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `feed_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `flight`
+--
+ALTER TABLE `flight`
+  MODIFY `flight_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `passenger_profile`
+--
+ALTER TABLE `passenger_profile`
+  MODIFY `passenger_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `pwdreset`
+--
+ALTER TABLE `pwdreset`
+  MODIFY `pwd_reset_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `ticket_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `flight`
+--
+ALTER TABLE `flight`
+  ADD CONSTRAINT `flight_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`);
+
+--
+-- Constraints for table `passenger_profile`
+--
+ALTER TABLE `passenger_profile`
+  ADD CONSTRAINT `passenger_profile_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `passenger_profile_ibfk_2` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`);
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `payment_ibfk_2` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`);
+
+--
+-- Constraints for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `ticket_ibfk_2` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`);
+-- Note: Removed passenger_id foreign key constraint to allow flexible ticket creation
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `routes`
+--
+
+CREATE TABLE `routes` (
+  `route_id` int(11) NOT NULL,
+  `source` varchar(20) NOT NULL,
+  `destination` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for table `routes`
+--
+ALTER TABLE `routes`
+  ADD PRIMARY KEY (`route_id`);
+
+--
+-- AUTO_INCREMENT for table `routes`
+--
+ALTER TABLE `routes`
+  MODIFY `route_id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `airline_operations`
+--
+
+CREATE TABLE `airline_operations` (
+  `operation_id` int(11) NOT NULL,
+  `flight_id` int(11) NOT NULL,
+  `baggage_revenue` int(11) NOT NULL DEFAULT 0,
+  `passenger_revenue` int(11) NOT NULL DEFAULT 0,
+  `flight_cost` int(11) NOT NULL DEFAULT 0,
+  `food_cost` int(11) NOT NULL DEFAULT 0,
+  `fuel_cost` int(11) NOT NULL DEFAULT 0,
+  `cancellation_ticket_loss` int(11) NOT NULL DEFAULT 0,
+  `technical_issues_cost` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for table `airline_operations`
+--
+ALTER TABLE `airline_operations`
+  ADD PRIMARY KEY (`operation_id`),
+  ADD KEY `flight_id` (`flight_id`);
+
+--
+-- AUTO_INCREMENT for table `airline_operations`
+--
+ALTER TABLE `airline_operations`
+  MODIFY `operation_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for table `airline_operations`
+--
+ALTER TABLE `airline_operations`
+  ADD CONSTRAINT `airline_operations_ibfk_1` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`);
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `skypanel`
+--
+
+CREATE TABLE `skypanel` (
+  `panel_id` int(11) NOT NULL,
+  `flight_id` int(11) NOT NULL,
+  `panel_name` varchar(50) NOT NULL,
+  `panel_type` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for table `skypanel`
+--
+ALTER TABLE `skypanel`
+  ADD PRIMARY KEY (`panel_id`),
+  ADD KEY `flight_id` (`flight_id`);
+
+--
+-- AUTO_INCREMENT for table `skypanel`
+--
+ALTER TABLE `skypanel`
+  MODIFY `panel_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for table `skypanel`
+--
+ALTER TABLE `skypanel`
+  ADD CONSTRAINT `skypanel_ibfk_1` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manifest`
+--
+
+CREATE TABLE `manifest` (
+  `manifest_id` int(11) NOT NULL,
+  `flight_id` int(11) NOT NULL,
+  `passenger_id` int(11) NOT NULL,
+  `seat_no` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for table `manifest`
+--
+ALTER TABLE `manifest`
+  ADD PRIMARY KEY (`manifest_id`),
+  ADD KEY `flight_id` (`flight_id`),
+  ADD KEY `passenger_id` (`passenger_id`);
+
+--
+-- AUTO_INCREMENT for table `manifest`
+--
+ALTER TABLE `manifest`
+  MODIFY `manifest_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for table `manifest`
+--
+ALTER TABLE `manifest`
+  ADD CONSTRAINT `manifest_ibfk_1` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`),
+  ADD CONSTRAINT `manifest_ibfk_2` FOREIGN KEY (`passenger_id`) REFERENCES `passenger_profile` (`passenger_id`);
